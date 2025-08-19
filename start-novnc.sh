d088cee28b0fe8b67882e3157715f3a1967807e6
nekomimi0128 <Nekomimi.0128@gmail.com>
2025-08-19 13:59:44 +0900

    Update start-novnc.sh

--- start-novnc.sh
+++ start-novnc.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# VNC パスワード設定（好きなパスワードを入れてね）
-VNC_PASS="nekomimi"
+# USER 環境変数を設定
+export USER=root
 
-# VNC サーバー起動（1ディスプレイ、指定パスワード）
-mkdir -p /root/.vnc
-echo $VNC_PASS | vncpasswd -f > /root/.vnc/passwd
-chmod 600 /root/.vnc/passwd
-vncserver :1 -geometry 1280x720 -depth 24 -rfbauth /root/.vnc/passwd
+# VNC サーバー起動（パスワード設定済み）
+vncserver :1 -geometry 1280x720 -depth 24
 
 # noVNC 起動
-/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 &
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
 
 echo "アクセスURL: http://localhost:6080/vnc.html?host=localhost&port=6080"
-echo "VNC パスワード: $VNC_PASS"
-wait
