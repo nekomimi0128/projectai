d84b2e4da93f9c2c336193c78e527e927ab93165
nekomimi0128 <Nekomimi.0128@gmail.com>
2025-08-20 11:15:21 +0000

    Edit

--- start-novnc.sh
+++ start-novnc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# 既存のセッションがあれば削除
-vncserver -kill :1 2>/dev/null || true
 
-# VNC 起動
+# VNCサーバー起動
 vncserver :1 -geometry 1280x720 -depth 24
 
 # noVNC 起動
-/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901
