92c6059add207c2a8c00a702eb59321ea15e0345
nekomimi0128 <Nekomimi.0128@gmail.com>
2025-08-20 11:00:14 +0000

    Edit

--- start-novnc.sh
+++ start-novnc.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+# 既存のセッションがあれば削除
+vncserver -kill :1 2>/dev/null || true
 
-# USER 環境変数を設定
-export USER=root
-
-# VNC サーバー起動（パスワード設定済み）
+# VNC 起動
 vncserver :1 -geometry 1280x720 -depth 24
 
 # noVNC 起動
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
-
-echo "アクセスURL: http://localhost:6080/vnc.html?host=localhost&port=6080"
+/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
